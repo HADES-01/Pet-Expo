34d9eb67608e82984ff0d18a85edba276ee05a9d
HADES-01 <adityavats5338@gmail.com>
2021-05-16 18:21:56 +0530

    added logo

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import img from "../../../public/assets/img/logo.png"
 import { Link } from 'react-router-dom';
+import './Header.scss';
 
 const Header = () => (
-  <header>
-    <Link to="/">Home</Link>
-    <img src={img} />
-    <nav>
-      <Link to="/helloworld">Hello World</Link>
-    </nav>
+  <header className = "header-flex-container">
+    <div className = "logo-flex-container">
+        <div className = "logo-img"><img src={img} /></div>
+        <div className = "logo-text">
+          <span>Pet Expo</span>
+        </div>
+    </div>
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/helloworld">Hello World</Link>
+      </nav>
+    </div>
 
     <hr />
   </header>
